72ecf217f2fb1f62deabcd6a3c48f4105403ce09
Suresh Reddy Guntaka <sureshreddy.guntaka@gmail.com>
2019-08-01 18:16:05 +0530

    Initial Commits

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    ['@vue/app', {
+      'useBuiltIns': 'entry',
+      'corejs': 3
+    }]
+  ],
+  plugins: [
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-syntax-export-default-from'
   ]
 }
